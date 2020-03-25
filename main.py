5f4003b521a028bcb6a24464bb2910f268105b0b
Permyak_Logy <maxika2004@mail.ru>
2020-03-25 16:14:17 +0300

    second mars

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from data import db_session, users, jobs
-from flask import Flask, render_template
+from flask import Flask
+from data import db_session
+from flask import render_template, redirect
+from data.users import User
+from data.jobs import Jobs
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
 import datetime
 
-User = users.User
-Jobs = jobs.Jobs
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = StringField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 def create_start_base():
@@ -88,6 +106,35 @@ def index():
     session = db_session.create_session()
     data = session.query(Jobs).all()
     return render_template("index.html", data=data)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой Login / email уже существует")
+        user = User(
+            email=form.email.data,
+            surname=form.name.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 def main():
